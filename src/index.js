d9a26e7f13835556ea617516455a27eead570b89
Aakanksha38 <chippa.aakanksha.16ce1023@gmail.com>
2024-03-08 08:12:38 +0530

    chap-2 props

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ root.render(<Greeting/>);
 */
 
 
-
+/*
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
 import nameStandImg from './img/nameStand.jpg';
@@ -79,4 +79,146 @@ const Image =()=> {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Catalogue/>);
+*/
 
+
+// Chapter =====2 
+
+// --->> how we can define component in 2 ways , writing style(inside 2 curly braces ) n js(inside the curly braces) under 
+/*
+import React from 'react';
+import  ReactDOM from 'react-dom/client';
+import './index.css';
+
+//this is also component 
+function BookList  () {
+  return (
+    <section>
+    <h1>Chapter 2</h1>
+    <Book></Book>
+    </section>
+  )
+}
+//this is also component 
+const author= 'Daniel Kahneman'; // global variable
+const Book =()=> {
+  const title = 'Thinking, fast and slow'; //local variable
+  return(
+    <article className='article'>
+      <img className='photo'
+        src= 'https://tse1.mm.bing.net/th?id=OIP.fIgWbZqX54jc0kQHnZ2UTwAAAA&pid=Api&P=0&h=180'
+        alt= 'Thinking, fast and slow'
+      />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      <p>Price: {100+50}</p>
+    </article>
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList/>)
+*/
+
+
+import React from 'react';
+import  ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+//this is also component 
+const firstBook={
+  img : "https://tse1.mm.bing.net/th?id=OIP.fIgWbZqX54jc0kQHnZ2UTwAAAA&pid=Api&P=0&h=180",
+  author: "Daniel Kahneman",
+  title:"Thinking, fast and slow"
+  }
+  const secondBook={
+    img :"https://tse3.mm.bing.net/th?id=OIP.NZ3INRcCAJ1XsXApbBJTHgHaLX&pid=Api&P=0&h=180",
+    author: "robin Sharma",
+    title:"The 5am club"
+    }
+  const checkAndLogical = true;
+  
+//this is also component 
+function BookList  () {
+  //what if iwant to pass more info to book ele? =>
+  return (
+    <section className='bookList'>
+    
+    <Book 
+      img={firstBook.img}
+      title={firstBook.title}
+      author={firstBook.author}
+      publishedYear ="2009" 
+      price="200">
+        <p>passing more info to this comp at children</p>
+      </Book>
+    <Book img={secondBook.img}
+      title={secondBook.title}
+      author={secondBook.author}
+      publishedYear ="2018"/>
+      <div>
+        {checkAndLogical  && <Book title="third book"/>}
+      </div>
+      
+    </section>
+  )
+}
+
+//access props- 1
+/*
+const Book =(props)=> {
+  console.log(props);
+  console.log(props.publishedYear);
+  //const title = 'Thinking, fast and slow'; //local variable
+  return(
+    <article className='article'>
+      
+      <img src={props.img} alt=''/>
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+      <p>{props.publishedYear}</p>
+    </article>
+  )
+}
+
+*/
+
+/*
+// access props- 2nd way/ DESTRUCTING PROPS
+const Book =(props) => {
+  //grouping all the props into curly braces n storing into the variable called props
+  const {img,title,author,publishedYear} = props;
+  return(
+    <article className='article'>
+      
+      <img src={img} alt=''/>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <p>{publishedYear}</p>
+    </article>
+  );
+  
+}
+*/
+
+//3rd way 
+//directly pass the props to function's parameter
+
+//use that children
+const Book =({img,title,author,publishedYear,children}) => {
+  return(
+    <article className='book'>
+      
+      <img src={img} alt=''/>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <p>{publishedYear}</p>
+      {children}
+    </article>
+  );
+  
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList/>)
